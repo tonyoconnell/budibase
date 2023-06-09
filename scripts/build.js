d044e09b8be0f80edd9f40c08d74f943e1bc956f
Adria Navarro <adria@budibase.com>
2023-06-09 11:47:39 +0100

    Fix dockerfile

--- scripts/build.js
+++ scripts/build.js
@@ -8,7 +8,6 @@ const path = require("path")
 
 const { build } = require("esbuild")
 
-const { default: NodeResolve } = require("@esbuild-plugins/node-resolve")
 const {
   default: TsconfigPathsPlugin,
 } = require("@esbuild-plugins/tsconfig-paths")
@@ -42,6 +41,8 @@ function runBuild(entry, outfile) {
       "koa2-ratelimit",
       "fsevents",
       "knex",
+      "pouchdb",
+      "testcontainers",
     ],
   }
 
